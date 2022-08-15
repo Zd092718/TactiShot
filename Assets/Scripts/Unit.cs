ae517e0ecb2dac5bbd072ce93fecc02f87b88261
Zack Dowd <zdowd2796@gmail.com>
2022-08-15 13:36:18 -0600

    animation transitions for moving added

--- Assets/Scripts/Unit.cs
+++ Assets/Scripts/Unit.cs
@@ -5,15 +5,26 @@ using UnityEngine;
 public class Unit : MonoBehaviour
 {
     private Vector3 targetPosition;
-    private float stoppingDistance = .1f;
+
+    [SerializeField] private Animator unitAnimator;
 
     private void Update()
     {
+        float stoppingDistance = .1f;
         if (Vector3.Distance(transform.position, targetPosition) > stoppingDistance)
         {
             Vector3 moveDirection = (targetPosition - transform.position).normalized;
             float moveSpeed = 4f;
             transform.position += moveDirection * moveSpeed * Time.deltaTime;
+
+            float rotateSpeed = 10f;
+            transform.forward = Vector3.Lerp(transform.forward, moveDirection, Time.deltaTime * rotateSpeed);
+
+            unitAnimator.SetBool("IsMoving", true);
+        }
+        else
+        {
+            unitAnimator.SetBool("IsMoving", false);
         }
 
         if (Input.GetMouseButtonDown(0))
