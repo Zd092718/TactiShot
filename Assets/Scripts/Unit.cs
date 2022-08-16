4389fcb63cd5fe72d47c22f62b83607201da62da
Zack Dowd <zdowd2796@gmail.com>
2022-08-16 11:07:09 -0600

    Level grid working properly

--- Assets/Scripts/Unit.cs
+++ Assets/Scripts/Unit.cs
@@ -5,12 +5,19 @@ using UnityEngine;
 public class Unit : MonoBehaviour
 {
     private Vector3 targetPosition;
+    private GridPosition gridPosition;
 
     [SerializeField] private Animator unitAnimator;
 
     private void Awake()
     {
         targetPosition = transform.position;
+    }
+
+    private void Start()
+    {
+        gridPosition = LevelGrid.Instance.GetGridPosition(transform.position);
+        LevelGrid.Instance.AddUnitAtGridPosition(gridPosition, this);
     }
 
     private void Update()
@@ -30,6 +37,13 @@ public class Unit : MonoBehaviour
         else
         {
             unitAnimator.SetBool("IsMoving", false);
+        }
+
+        GridPosition newGridPosition = LevelGrid.Instance.GetGridPosition(transform.position);
+        if (newGridPosition != gridPosition)
+        {
+            LevelGrid.Instance.UnitMovedGridPosition(this, gridPosition, newGridPosition);
+            gridPosition = newGridPosition;
         }
 
     }
