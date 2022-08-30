63c3abd2446eefe5a3d8b37271f24ff4d5699fba
Zack Dowd <zdowd2796@gmail.com>
2022-08-29 20:16:52 -0600

    Added pathfinding testing

--- Assets/Scripts/Testing.cs
+++ Assets/Scripts/Testing.cs
@@ -15,7 +15,20 @@ public class Testing : MonoBehaviour
     {
         if (Input.GetKeyDown(KeyCode.T))
         {
+            GridPosition mouseGridPosition = LevelGrid.Instance.GetGridPosition(MouseWorld.GetPosition());
+            GridPosition startGridPostion = new GridPosition(0, 0);
 
+            List<GridPosition> gridPositionList = Pathfinding.Instance.FindPath(startGridPostion, mouseGridPosition);
+
+            for (int i = 0; i < gridPositionList.Count - 1; i++)
+            {
+                Debug.DrawLine(
+                    LevelGrid.Instance.GetWorldPosition(gridPositionList[i]),
+                    LevelGrid.Instance.GetWorldPosition(gridPositionList[i + 1]),
+                    Color.white,
+                    10f
+                );
+            }
 
         }
     }
